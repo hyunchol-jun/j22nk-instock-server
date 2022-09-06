2e13f4212b9c43f3bf59d221494cff4e41e0b60f
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-06 15:16:17 -0400

    Add get, put, post endpoints on /warehouses/:warehousesId

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -5,4 +5,15 @@ router.get("/", (req, res) => {
     res.send("Get request to warehouses");
 })
 
+router.route("/:warehouseId")
+    .get((req, res) => {
+    res.send("Get request to warehouse ID: " + req.params.warehouseId);
+})
+    .put((req, res) => {
+    res.send("Put request to warehouse ID: " + req.params.warehouseId);
+})
+    .post((req, res) => {
+    res.send("Post request to warehouse ID: " + req.params.warehouseId);
+})
+
 module.exports = router;
