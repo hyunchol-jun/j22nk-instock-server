c432bfb67e7d9fcb8e928fcc32676cbc8b4d7c7f
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-06 15:21:32 -0400

    Add get, put, post endpoints on /inventories/:inventoriesId

--- routes/inventories.js
+++ routes/inventories.js
@@ -5,4 +5,15 @@ router.get("/", (req, res) => {
     res.send("Get request to inventories");
 })
 
+router.route("/:inventoryId")
+    .get((req, res) => {
+    res.send("Get request to inventory ID: " + req.params.inventoryId);
+})
+    .put((req, res) => {
+    res.send("Put request to inventory ID: " + req.params.inventoryId);
+})
+    .post((req, res) => {
+    res.send("Post request to inventory ID: " + req.params.inventoryId);
+})
+
 module.exports = router;
