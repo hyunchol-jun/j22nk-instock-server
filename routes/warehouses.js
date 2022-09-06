dac00da2eb312ec43764e58c1bb47701093b847e
Janet Leung <leung.janetw@hotmail.com>
2022-09-06 17:10:07 -0400

    Creates GET request for all warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
+function readWarehouses() {
+    const warehousesFile = fs.readFileSync("./data/warehouses.json");
+    const warehousesData = JSON.parse(warehousesFile);
+    return warehousesData;
+}
+
+// GET all warehouses
 router.route("/")
     .get((req, res) => {
-        res.send("Get request to warehouses");
+        const warehouses = readWarehouses();
+        const listOfWarehouses = warehouses.map((warehouse) => {
+            return (
+                {
+                    "id": warehouse.id,
+                    "name": warehouse.name,
+                    "address": warehouse.address,
+                    "city": warehouse.city,
+                    "country": warehouse.country,
+                    "contact": {
+                        "name": warehouse.contact.name,
+                        "position": warehouse.contact.position,
+                        "phone": warehouse.contact.phone,
+                        "email": warehouse.contact.email
+                    }
+                }
+            )
+        })
+        res.status(200).json(listOfWarehouses);
     })
     .post((req, res) => {
         res.send("Post request to warehouses");
