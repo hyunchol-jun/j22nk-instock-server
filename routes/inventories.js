80bb8dee0050d25b17331740062fd6a0038a8568
Moin Sekander <moinuddin8510@gmail.com>
2022-09-08 00:37:50 -0400

    implement warehouse delete by id route, which also deletes associated inventories

--- routes/inventories.js
+++ routes/inventories.js
@@ -20,9 +20,9 @@ router.route("/")
 router.route("/:inventoryId")
     .get((req, res) => {
     res.send("Get request to inventory ID: " + req.params.inventoryId);
-})
+    })
     .put((req, res) => {
     res.send("Put request to inventory ID: " + req.params.inventoryId);
-})
+    })
 
 module.exports = router;
